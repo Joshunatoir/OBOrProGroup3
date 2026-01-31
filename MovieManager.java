df9130770a8fb5026bb48e577e45c7fab8422818
Julianalva20 <Julian.Knightalvarez@edu.sait.ca>
2026-01-30 21:08:12 -0700

    Add files via upload
    updated the generatemovielistinyear

--- MovieManager.java
+++ MovieManager.java
@@ -13,15 +13,25 @@ public class MovieManager {
 		ArrayList<Movie> movieList = new ArrayList<Movie>();
 		
 		String line;
-		String [] fields;
+		String[] fields;
 		Movie data;
 		try(BufferedReader in = new BufferedReader(new FileReader(fileName))){
 			line = in.readLine();
 			while(line != null) {
+				line = line.trim();
+				if (line.isBlank()) { // quick end instead of run through the whole loop again 
+					continue;
+				}
 				fields = line.split(delimiter);
-				data = new Movie(Integer.parseInt(fields[0]),(fields[1]),Integer.parseInt(fields[2]));
-				movieList.add(data);//adding data to the list
-				line = in.readLine();//preping the while loop to either continue with the next line or grabing nothing form the file to end the loop 				
+				if (fields.length == 3){
+					int duration = Integer.parseInt(fields[0].trim());
+					String title = (fields[1].trim());
+					int year = Integer.parseInt(fields[2].trim());
+					data = new Movie(duration,title,year);
+					movieList.add(data);//adding data to the list
+					line = in.readLine();//preping the while loop to either continue with the next line or grabing nothing form the file to end the loop
+					//System.out.println(line);//testing purposes
+				}
 			}
 		
 		}catch (IOException e) {
@@ -29,6 +39,7 @@ public class MovieManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//System.out.println(movieList);//testing purposes
 		return movieList;
 		
 	}
@@ -36,12 +47,10 @@ public class MovieManager {
 	public static void saveMovieListToFile(ArrayList<Movie> movieList) {
 		if(movieList != null) {
 			try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))){
+				//System.out.println(movieList); //testing purposes
 				for(Movie m : movieList) {
-					out.print(m.getDuration()+ delimiter);//print these things to file
-					out.print(m.getTitle()+ delimiter);
-					out.print(m.getYear());
+					out.print(m.getDuration()+ delimiter +m.getTitle() + delimiter + m.getYear());//print these things to file
 					out.println();
-					
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -50,62 +59,66 @@ public class MovieManager {
 		}
 		
 	}
+	
+    public static void generateMovieListInYear(ArrayList<Movie> movieList, Scanner sc) {
+
+        System.out.println("\nEnter in year: ");
+        int year = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("\nMovie List");
+        System.out.printf("%-10s %-6s %s%n", "Duration", "Year", "Title");
+
+
+        int totalDuration = 0;
+
+        for (Movie m : movieList) {
+            if (m.getYear() == year) {
+                System.out.printf("%-10d %-6d %s%n",
+                        m.getDuration(),
+                        m.getYear(),
+                        m.getTitle());
+                totalDuration += m.getDuration();
+            }
+        }
+
+        System.out.println("Total duration: " + totalDuration + " minutes\n");
+    }
 
 	public static void generateRandomMovieList(ArrayList<Movie> movieList) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many movies should be listed: ");
 		int quantity = Integer.parseInt(sc.nextLine());
-		
-		if (quantity <= 0 || quantity > movieList.size()) {
-			System.out.println("Invalid number.");
-			return;
-		}
-		
-		int totalDuration = 0;
-		Random rand = new Random();
-		
-		System.out.println("\nDuration   Year   Title");
-		
-		for (int i = 0; i < quantity; i++) {
-			int index = rand.nextInt(movieList.size());
-			Movie m = movieList.get(index);
-			
-			System.out.println(m);
-			totalDuration += m.getDuration();
-		}
-		
-		System.out.println("\nTotal duration: " + totalDuration + " minutes");
 	}
-	
 
 	
 	public static void addMovie(ArrayList<Movie> movieList) {
-		try (Scanner sc = new Scanner(System.in)) {
-			boolean loop = true;
-			while (loop == true) {
-				try {
-				System.out.println("Enter Duration: ");
-				int duration = Integer.parseInt(sc.nextLine().trim());
-				System.out.println("Enter Movie Title: ");
-				String title = sc.nextLine().trim();
-				System.out.println("Enter Movie Year:  ");
-				int year = Integer.parseInt(sc.nextLine().trim());
-				if (title.isBlank()) {
-					continue;
-				}
-				Movie data;
-				data = new Movie((duration),(title),(year));
-				movieList.add(data);
-				loop = false;
-				}catch (NumberFormatException e) {
-					System.out.println("Invalid input! Please try again.");
-				}
-				saveMovieListToFile(movieList);
+		Scanner sc = new Scanner(System.in);
+		boolean loop = true;
+		while (loop == true) {
+			try {
+			System.out.println("Enter Duration: ");
+			int duration = Integer.parseInt(sc.nextLine().trim());
+			System.out.println("Enter Movie Title: ");
+			String title = sc.nextLine().trim();
+			System.out.println("Enter Movie Year:  ");
+			int year = Integer.parseInt(sc.nextLine().trim());
+			if (title.isBlank()) {
+				continue;
+			}
+			Movie data;
+			data = new Movie((duration),(title),(year));
+			movieList.add(data);
+			loop = false;
+			}catch (NumberFormatException e) {
+				System.out.println("Invalid input! Please try again.");
 			}
 		}
+		saveMovieListToFile(movieList);
 		System.out.println("Saving Movies. . .");
 		System.out.println("Added movie to the data file.");
 	}
+	
 	public static void displayMenu(ArrayList<Movie> movieList) {
 		Scanner sc = new Scanner(System.in);
 		boolean run = true;
@@ -120,17 +133,17 @@ public class MovieManager {
 			if(choice == 1) {
 				addMovie(movieList);
 			}else if (choice == 2) {
-				generateMovieListInYear(movieList);
+				generateMovieListInYear(movieList, sc);
 			}else if (choice == 3) {
 				generateRandomMovieList(movieList);
 			}else if (choice == 4) {
+				saveMovieListToFile(movieList);
 				System.out.println("Goodbye!");
 				run = false;
 			}else {
 				System.out.println("Invalid Option!");
-				
 			}
+			
 		}
 	}
 }
-
